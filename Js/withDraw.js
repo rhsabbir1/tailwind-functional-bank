a18e20ee6efba9fec2d35ede7fb48e30c0f48f63
rhsabbir1 <rhsabbir090@gmai.com>
2023-02-12 22:25:12 +0600

    withDreaw bakance update

--- Js/withDraw.js
+++ Js/withDraw.js
@@ -7,5 +7,20 @@ document.getElementById('withdrow-btn').addEventListener('click',function(){
     if(isNaN(withdrawAmount)){
         return;
     }
+
+    const withdradTotalElement =document.getElementById('withdrad-total');
+    const withdradStr = withdradTotalElement.innerText;
+    const previousWithdrawAmount = parseFloat(withdradStr)
+
+    const newWithdrawAmount = previousWithdrawAmount + withdrawAmount;
+    withdradTotalElement.innerText = newWithdrawAmount;
+
+
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalString = balanceTotal.innerText;
+    const previousBalanceAmount = parseFloat(balanceTotalString)
+
+    const newbalance = previousBalanceAmount - withdrawAmount;
+    balanceTotal.innerText = newbalance;
     
 })
