8bdbce894ef002472ae50a926543363d10797c5a
rhsabbir1 <rhsabbir090@gmai.com>
2023-02-13 16:59:15 +0600

    Deposite Balance done

--- functional-js/deposite2.js
+++ functional-js/deposite2.js
@@ -1,1 +1,19 @@
-console.log(55)
+
+
+document.getElementById('depsite-btn').addEventListener('click',function(){
+    
+    const newDepositeAmount = getInputFieldValue('deposite-field');
+    if(isNaN(newDepositeAmount)){
+        return;
+    }
+
+    const previousDepositeAmount = getElementValueByID("deposite-total");
+    const totalDepositeAmount = previousDepositeAmount + newDepositeAmount;
+
+    setNewValueByID('deposite-total', totalDepositeAmount);
+
+    const totalBalance = getElementValueByID('balance-total')
+    const newBlanceTotal = totalBalance + newDepositeAmount;
+    setNewValueByID('balance-total' , newBlanceTotal)
+
+})
--- functional-js/utilites.js
+++ functional-js/utilites.js
@@ -1,1 +1,19 @@
-console.log(55)
+function getInputFieldValue(filedId){
+    const fieldvalue = document.getElementById(filedId);
+    const fieldvalueStr = fieldvalue.value;
+    const fieldValueAmount = parseFloat(fieldvalueStr);
+    fieldvalue.value ='';
+    return fieldValueAmount;
+}
+
+function getElementValueByID(elementId){
+    const elementValue = document.getElementById(elementId);
+    const elementStr =elementValue.innerText;
+    const elementAmount = parseFloat(elementStr);
+    return elementAmount;
+}
+
+function setNewValueByID(elmentID , value){
+    const amountId = document.getElementById(elmentID);
+    amountId.innerText = value;
+}
