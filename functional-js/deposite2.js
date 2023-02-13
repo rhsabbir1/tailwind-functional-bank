702948d94b105ffd33048dd91ba9da240051419a
rhsabbir1 <rhsabbir090@gmai.com>
2023-02-13 17:28:50 +0600

    validation for NaN

--- functional-js/deposite2.js
+++ functional-js/deposite2.js
@@ -4,6 +4,7 @@ document.getElementById('depsite-btn').addEventListener('click',function(){
     
     const newDepositeAmount = getInputFieldValue('deposite-field');
     if(isNaN(newDepositeAmount)){
+        alert('Provide a Valid Amount')
         return;
     }
 
--- functional-js/withDraw.js
+++ functional-js/withDraw.js
@@ -1,11 +1,21 @@
 document.getElementById('withdrow-btn').addEventListener('click',function(){
     const newWithDraw = getInputFieldValue('Withdrow-field');
     if(isNaN(newWithDraw)){
+        alert('Provide a Valid Amount')
         return;
     }
     
     const previousDepositeAmount = getElementValueByID('withdrad-total');
 
+    const previousBalance = getElementValueByID('balance-total');
+    if(newWithDraw> previousBalance){
+        alert('Provide a Valid Amount')
+        return;
+    }
+
     const totalWithdraw = previousDepositeAmount + newWithDraw;
     setNewValueByID('withdrad-total' , totalWithdraw)
+
+    const newBalance = previousBalance - newWithDraw;
+    setNewValueByID('balance-total' , newBalance)
 })
